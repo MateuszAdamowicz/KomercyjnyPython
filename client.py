0f6340da10218bee6fd5d847b69e08fd172aeaa3
Mateusz Adamowicz <adammatb@gmail.com>
2012-12-30 16:27:54 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,122 +1,125 @@
-#!/usr/bin/env python 
-import pygame, select, cPickle
+#!/usr/bin/env python
+import select, socket, sys, cPickle, time, pygame
+from protocolObjects import *
 from pygame.locals import *
-from sys import exit
 from socket import *
 
+
 class gracz(pygame.sprite.Sprite):
-  
-  def __init__(self):
-    pygame.sprite.Sprite.__init__(self)
-    if len(sys.argv) != 2:
-      raise AttributeError("Incorrect number of arguments. You should pass ip:port as only argument.")
-    host,port = sys.argv[1].split(':')
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		if len(sys.argv) != 2:
+	  		raise AttributeError("Incorrect number of arguments. You should pass ip:port as only argument.")
+		host,port = sys.argv[1].split(':')
 
-    self.port = int(port)
-    self.host = host
-    self.soc = socket(AF_INET, SOCK_STREAM)
-    self.soc.connect((self.host, self.port))
-    self.soc.setblocking(0)
-    print "Connected to %s:%d" % (self.host, self.port)
-    self.ID = None
-    self.run = True
-    self.resss = True
+		self.port = int(port)
+		self.host = host
+		self.soc = socket(AF_INET, SOCK_STREAM)
+		self.soc.connect((self.host, self.port))
+		self.soc.setblocking(0)
+		print "Connected to %s:%d" % (self.host, self.port)
+		self.ID = None
+		self.run = True
+		self.resss = True
   
-  def send(self,dane):
-    _, toWrite, _ = select.select([], [self.soc], [])
-    data = toWrite[0].sendall( cPickle.dumps( dane ) )
-    if data: raise Exception("Problem z wyslaniem msg do socketu: %s" %data)
+  	def send(self,dane):
+		_, toWrite, _ = select.select([], [self.soc], [])
+		data = toWrite[0].sendall( cPickle.dumps( dane ) )
+		if data: raise Exception("Problem z wyslaniem msg do socketu: %s" %data)
   
-  def receive(self):
-    toRead, _, _ = select.select([self.soc], [], [])
-    rec = toRead[0].recv(4096)
-    if len(rec) == 0:
-      return None
-    data = None
-    while data == None:
-      try:
-	data = cPickle.loads( rec )
-      except (EOFError, cPickle.UnpicklingError, ValueError):
-	rec += toRead[0].recv(4096)
-    return data
-    
-  def draw(self,scr,i,pos):
-    i += 1
-    if i == 1:
-      os = pygame.image.load('gr.png').convert_alpha()
-    else:
-      os = pygame.image.load('gr2.png').convert_alpha()
-    scr.blit(os, pos)
-    
-  def update(self):
-    self.rect.centerx = self.x
-    self.rect.centery = self.y
+  	def receive(self):
+		toRead, _, _ = select.select([self.soc], [], [])
+		rec = toRead[0].recv(4096)
+		if len(rec) == 0:
+	  		return None
+		data = None
+		while data == None:
+	  		try:
+				data = cPickle.loads( rec )
+	  		except (EOFError, cPickle.UnpicklingError, ValueError):
+				rec += toRead[0].recv(4096)
+		return data
+	
+  	def draw(self,scr,i,pos):
+		i += 1
+		if i == 1:
+	  		os = pygame.image.load('gr.png').convert_alpha()
+		else:
+	  		os = pygame.image.load('gr2.png').convert_alpha()
+		scr.blit(os, pos)
+	
+  	def update(self):
+		self.rect.centerx = self.x
+		self.rect.centery = self.y
 
-  def start(self):
-    clock = pygame.time.Clock()
-    while self.run:
-      clock.tick(15)
-      data = self.receive()
-      if data == None: continue
-      if isinstance(data,Countdown):
-        if data.number == 3:
-          pygame.init()
-          screen = pygame.display.set_mode((data.mapSize[0], data.mapSize[1]))
-          self.ID = data.playerID
-          pygame.display.set_caption("gracz " + str(self.ID+1))
-          font = pygame.font.Font("CatShop.ttf", 72)
-        tekst = font.render(str(data.number),True,(0,0,0))
-        screen.fill((250,250,250))
-        screen.blit(tekst,(300,200))
-        time.sleep(1)
-        PlayerAction.action = 'n'
+  	def start(self):
+		clock = pygame.time.Clock()
+		while self.run:
+	  		clock.tick(15)
+	  		data = self.receive()
+	  		if data == None: continue
+	  		if isinstance(data,Countdown):
+				if data.number == 3:
+		  			pygame.init()
+		  			screen = pygame.display.set_mode((data.mapSize[0], data.mapSize[1]))
+		  			self.ID = data.playerId
+		  			pygame.display.set_caption("gracz " + str(self.ID+1))
+		  			font = pygame.font.Font("CatShop.ttf", 72)
+				tekst = font.render(str(data.number),True,(0,0,0))
+				screen.fill((250,250,250))
+				screen.blit(tekst,(300,200))
+				time.sleep(1)
+				PlayerAction.action = 'n'
 
-      elif isinstance(data,Map):
-        warstwa = pygame.image.load('plik.jpg').convert_alpha()
-        b = pygame.image.load('mina.png').convert_alpha()
-        for i in data.mines:
-          warstwa.blit(b,i.position)
-        for i,pos in enumerate(data.playersPositions):
-          self.draw(warstwa,(i,pos.x,pos.y))
-        pressed_keys = pygame.key.get_pressed()
-        for event in pygame.event.get():
-          if event.type == QUIT:
-            self.send('koniec')
-            exit()
-    		if pressed_keys[K_LEFT]:
-    		    PlayerAction.action = 'left'
-    		elif pressed_keys[K_RIGHT] :
-    		    PlayerAction.action = 'right'
-    		elif pressed_keys[K_UP]:
-    		    PlayerAction.action = 'up'
-    		elif pressed_keys[K_DOWN]:
-    		    PlayerAction.action = 'down'
-    		elif pressed_keys[K_LCTRL]:
-    		    PlayerAction.action = 'result'
-    		elif pressed_keys[K_SPACE]:
-    		    PlayerAction.action = 'bomb'
-    		else:
-    		    PlayerAction.action = 'none'
-    		screen.blit(warstwa,(0,0))
-    		self.send(PlayerAction.action)
-      
-      elif isinstance(data,Result):
-        tekst = font.render('Koniec',True, (30,100,200))
-        tekst2 = font.render('Wygral gracz: ' + str(data.winners + 1), True, (0,0,0))
-        tekst3 = font.render('Wynik: ' + str(data.scores), True, (0,0,0))
-        screen.fill((250,250,250))
-        screen.blit(tekst,(50,50))
-        screen.blit(tekst2,(50,200))
-        screen.blit(tekst3,(50,300))
-        pygame.display.update()
-        time.sleep(10)
-        self.resss = False
-        self.run = False
-        break
-      elif data == 'koniec':
-        break
-      pygame.display.update()
-    if self.resss:
-      self.send('koniec')
-    self.soc.close()
-    exit()
+	  		elif isinstance(data,Map):
+				warstwa = pygame.image.load('plik.jpg').convert_alpha()
+				b = pygame.image.load('gr.png').convert_alpha()
+				for i in data.mines:
+		  			warstwa.blit(b,i.position)
+				for i,pos in enumerate(data.playersPositions):
+		  			self.draw(warstwa,(i,pos.x,pos.y))
+				pressed_keys = pygame.key.get_pressed()
+				for event in pygame.event.get():
+		  			if event.type == QUIT:
+						self.send('koniec')
+						exit()
+				if pressed_keys[K_LEFT]:
+					PlayerAction.action = 'left'
+				elif pressed_keys[K_RIGHT] :
+					PlayerAction.action = 'right'
+				elif pressed_keys[K_UP]:
+					PlayerAction.action = 'up'
+				elif pressed_keys[K_DOWN]:
+					PlayerAction.action = 'down'
+				elif pressed_keys[K_LCTRL]:
+					PlayerAction.action = 'result'
+				elif pressed_keys[K_SPACE]:
+					PlayerAction.action = 'bomb'
+				else:
+					PlayerAction.action = 'none'
+				screen.blit(warstwa,(0,0))
+				self.send(PlayerAction.action)
+	  
+	  		elif isinstance(data,Result):
+				tekst = font.render('Koniec',True, (30,100,200))
+				tekst2 = font.render('Wygral gracz: ' + str(data.winners + 1), True, (0,0,0))
+				tekst3 = font.render('Wynik: ' + str(data.scores), True, (0,0,0))
+				screen.fill((250,250,250))
+				screen.blit(tekst,(50,50))
+				screen.blit(tekst2,(50,200))
+				screen.blit(tekst3,(50,300))
+				pygame.display.update()
+				time.sleep(10)
+				self.resss = False
+				self.run = False
+				break
+	  		elif data == 'koniec':
+				break
+	  		pygame.display.update()
+		if self.resss:
+	  		self.send('koniec')
+		self.soc.close()
+		exit()
+
+p = gracz()
+p.start()
